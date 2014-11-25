ef3ed6111e9d39fad480973ae7fa1b0544699c06
GitOutATown <richard@aqume.com>
2014-11-25 09:39:30 -0800

    adding comment

--- src/main/scala/lab/math/EuclideanLabBreeze2.scala
+++ src/main/scala/lab/math/EuclideanLabBreeze2.scala
@@ -12,13 +12,13 @@ object EuclideanLabBreeze2 extends App {
 	val B = DenseVector(3.6, 5.0, 6.3, 7.9)
 	val C = DenseVector(-3.6, -5.0, -6.3, -7.9)
 	
-	// Computes Euclidean distance ////////////////////////////////////////
+	///// Computes Euclidean distance ////////////////////////////////////////
 	def eucDist(X: DenseVector[Double], Y: DenseVector[Double]): Double = {
   		require(X.length == X.length)
   		sqrt(pow(X - Y, 2).sum)
 	}
 	
-	//...................................//
+	//.. tests .................................//
 	
 	val res1 = eucDist(A, B)
 	println("res1: " + res1) // 2.651414716712571
@@ -26,12 +26,12 @@ object EuclideanLabBreeze2 extends App {
 	val res2 = eucDist(A, C)
 	println("res2: " + res2)
 
-	// Computes Euclidean norm ////////////////////////////////////////////
+	///// Computes Euclidean norm ////////////////////////////////////////////
 	def eucNorm(X: DenseVector[Double]): Double = {
 		sqrt(pow(X, 2).sum)
 	}
 	
-	//...........................//
+	//.. tests .........................//
 	
 	val res3 = eucNorm(A)
 	println("res3: " + res3) // 9.211405973031479
@@ -39,14 +39,14 @@ object EuclideanLabBreeze2 extends App {
 	val res4 = eucNorm(C)
 	println("res4: " + res4) // 11.83469475736489
 	
-	// Minkowski norm /////////////////////////////////////////////
+	///// Minkowski norm /////////////////////////////////////////////
 	
 	def minkNorm(X0: DenseVector[Double], n: Int): Double = {
 		val X1 = if(n % 2 == 0) X0 else X0.map(x => abs(x)) // TODO: This seems heavy
 		nthRoot(pow(X1, n).sum, n)
 	}
 	
-	//........................//
+	//.. tests ......................//
 	
 	println("res5: " + minkNorm(A, 3))
 	println("res6: " + minkNorm(A, 4))
